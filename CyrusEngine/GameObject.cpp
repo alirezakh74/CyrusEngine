a611bb2f66c7e760cfc9c744c0bad52377eee305
Alireza <alirezakhodabande74@gmail.com>
2021-05-16 14:07:27 +0430

    add feature for anim, movement, size, rotation, blending, , update, collision and type

--- CyrusEngine/GameObject.cpp
+++ CyrusEngine/GameObject.cpp
@@ -1,6 +1,8 @@
 #include "GameObject.h"
 
-GameObject::GameObject()
+GameObject::GameObject() : m_position(0, 0), m_velocity(0, 0), m_acceleration(0, 0)
+, m_width(0), m_height(0), m_currentRow(0), m_currentFrame(0), m_numFrames(0)
+, m_bUpdating(false), m_bDead(false), m_bDying(false), m_angle(0), m_alpha(255)
 {
 }
 
--- CyrusEngine/GameObject.h
+++ CyrusEngine/GameObject.h
@@ -1,20 +1,56 @@
 #pragma once
 
 #include "LoaderParams.h"
+#include <memory>
+#include "Vector2D.h"
 
 class GameObject
 {
-public:
 
-	virtual void load(const LoaderParams* pParams) = 0;
+public:
+	// movement variables
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+	// size variables
+	int m_width;
+	int m_height;
+	// animation variables
+	int m_currentRow;
+	int m_currentFrame;
+	int m_numFrames;
+	std::string m_textureID;
+	// common boolean variables
+	bool m_bUpdating;
+	bool m_bDead;
+	bool m_bDying;
+	// rotation
+	double m_angle;
+	// blending
+	int m_alpha;
+
+public:
+	virtual ~GameObject();
+
+	virtual void load(std::unique_ptr<LoaderParams> const &pParams) = 0;
 	virtual void update() = 0;
 	virtual void draw() = 0;
 	virtual void clean() = 0;
 
+	virtual void collision() = 0;
+	virtual std::string type() = 0;
+	Vector2D& getPosition() { return m_position; }
+	int getWidth() const { return m_width; }
+	int getHeight() const { return m_height; }
+	// void scroll(float scrollSpeedX, scrollSpeedY)
+	bool updating() const { return m_bUpdating; }
+	bool dead() const { return m_bDead; }
+	bool dying() const { return m_bDying; }
+	void setUpdating(bool updating) { m_bUpdating = updating; }
+
 protected:
 
 	GameObject();
-	virtual ~GameObject();
 
 	/*GameObject(const LoaderParams* pParams);
 	virtual ~GameObject();*/
