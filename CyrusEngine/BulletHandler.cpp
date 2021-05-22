a434527d3bcf1f60f69dcb68a789a2ccec26b5e5
Alireza <alirezakhodabande74@gmail.com>
2021-05-22 20:26:06 +0430

    bullet manager

---
+++ CyrusEngine/BulletHandler.cpp
@@ -0,0 +1,25 @@
+#include "BulletHandler.h"
+
+void BulletHandler::addPlayerBullet(int x, int y, int width, int height, std::string textureID, int numFrames, Vector2D heading)
+{
+}
+
+void BulletHandler::addEnemyBullet(int x, int y, int width, int height, std::string textureID, int numFrames, Vector2D heading)
+{
+}
+
+void BulletHandler::updateBullets()
+{
+}
+
+void BulletHandler::drawBullets()
+{
+}
+
+void BulletHandler::clearBullets()
+{
+}
+
+BulletHandler::~BulletHandler()
+{
+}
