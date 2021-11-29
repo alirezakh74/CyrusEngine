7c1fc563526638a6f3e9827861d9b32f72decb0e
alireza khodabande <alirezakhodabande74@gmail.com>
2021-11-29 23:42:34 +0330

    clear vectors

--- CyrusEngine/Level.cpp
+++ CyrusEngine/Level.cpp
@@ -16,12 +16,23 @@ void Level::render()
 	}
 }
 
+void Level::clear()
+{
+	for (int i = 0; i < m_layers.size(); ++i)
+	{
+		delete (m_layers[i]);
+	}
+	m_layers.clear();
+
+	m_tilesets.clear();
+}
+
 Level::Level()
 {
 }
 
 Level::~Level()
 {
-	m_tilesets.clear();
-	m_layers.clear();
+	//m_tilesets.clear();
+	//m_layers.clear();
 }
